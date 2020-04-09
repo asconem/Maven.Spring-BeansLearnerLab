9cfcb9bec97253ca601c96c82fd7c0cbd2293e45
asconem <asconem@gmail.com>
2020-04-09 17:07:11 -0400

    Adding to Instructor class

--- src/main/java/com/example/demo/Instructor.java
+++ src/main/java/com/example/demo/Instructor.java
@@ -1,17 +1,34 @@
 package com.example.demo;
 
+import java.util.Iterator;
+
 public class Instructor extends Person implements Teacher {
+
+    public Instructor(Long id, String name) {
+        super(id, name);
+    }
+
     @Override
     public void teach(Learner learner, double numberOfHours) {
-
+            learner.learn(numberOfHours);
     }
 
     @Override
     public void lecture(Iterable<? extends Learner> learners, double numberOfHours) {
-        double numberOfHoursPerLearner = 0;
+        double numberOfHoursPerLearner;
 
-        for (Learner element : learners) {
-            element.learn(numberOfHoursPerLearner);
+        Iterator<? extends Learner> iterator = learners.iterator();
+
+        int length = 0;
+
+        while (iterator.hasNext()){
+            length++;
+            iterator.next();
+        }
+        numberOfHoursPerLearner = numberOfHours / length;
+
+        for (Learner l : learners){
+            l.learn(numberOfHoursPerLearner);
         }
     }
 }
